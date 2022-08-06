a055f795cb835cf69c6a89b46f22f8a3091c6af7
Hirantha Ranathunga <67953132+ghstkillrD@users.noreply.github.com>
2022-08-06 13:45:27 +0530

    Js file committed

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+
+
+function cheack password(){
+if(document.getelemeentById("pwd").value!=document.getElemeentById("cpwd").value){
+	alert=("Password Mismatch");
+	return false;
+}else("Password Match")
+
+}
+
+function enableButtonn(){
+	if(document.getElementById("checkBox").checked){
+	
+	(document.getElementById("submitbtn").disabled)
+	}
+
+}
---
+++ JS/tourism_news_script.js
@@ -0,0 +1,56 @@
+function openForm7() {
+  document.getElementById("1").style.display = "block";
+}
+
+function closeForm7() {
+  document.getElementById("1").style.display = "none";
+}
+
+function openForm5() {
+  document.getElementById("3").style.display = "block";
+}
+
+function closeForm5() {
+  document.getElementById("3").style.display = "none";
+}
+
+function openForm4() {
+  document.getElementById("4").style.display = "block";
+}
+
+function closeForm4() {
+  document.getElementById("4").style.display = "none";
+}
+
+function openForm3() {
+  document.getElementById("5").style.display = "block";
+}
+
+function closeForm3() {
+  document.getElementById("5").style.display = "none";
+}
+
+function openForm2() {
+  document.getElementById("6").style.display = "block";
+}
+
+function closeForm2() {
+  document.getElementById("6").style.display = "none";
+}
+
+function openForm1() {
+  document.getElementById("7").style.display = "block";
+}
+
+function closeForm1() {
+  document.getElementById("7").style.display = "none";
+}
+
+function openForm() {
+  document.getElementById("8").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("8").style.display = "none";
+}
+
